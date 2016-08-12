92c2320b8483840bb5c61f365c06fcca86e53c4d
tufedtm <tufedtm@gmail.com>
2016-08-12 12:53:30 +0300

    Update 2016-08-12T09:53:25.507Z

--- static/js/app.js
+++ static/js/app.js
@@ -2,9 +2,11 @@
 
 $(function () {
 
-  $.ionTabs('#tabs_1', {
-    type: 'storage'
-  });
+  if ($.ionTabs) {
+    $.ionTabs('#tabs_1', {
+      type: 'none'
+    });
+  }
 
   $('[data-js-code-reg-btn]').on('click', function () {
     $(this).addClass('tab2__btn--hide');
