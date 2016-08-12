af8bc2c763a653c2468391869613bf88d80a8c24
tufedtm <tufedtm@gmail.com>
2016-08-12 21:29:42 +0300

    + rules

--- tasks/gh_pages.js
+++ tasks/gh_pages.js
@@ -2,10 +2,19 @@
 
 import gulp from 'gulp'
 import ghPages from 'gulp-gh-pages'
+import runSequence from 'run-sequence'
 
-gulp.task('gh_pages', ['clear', 'template', 'style', 'script', 'static'], function() {
+gulp.task('gh_pages:src', function() {
   return gulp.src('./dist/**/*')
     .pipe(ghPages({
       remoteUrl : 'https://github.com/tufedtm/perfect_fit.git'
     }));
+});
+
+gulp.task('gh_pages', function () {
+  runSequence(
+    'clear',
+    ['template', 'style', 'script', 'static'],
+    'gh_pages:src'
+  );
 });
