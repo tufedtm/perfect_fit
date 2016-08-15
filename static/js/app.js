527a8f33a7b852cc6206153dfcad3de2dd1d9999
tufedtm <tufedtm@gmail.com>
2016-08-15 16:20:25 +0300

    Update 2016-08-15T13:20:23.006Z

--- static/js/app.js
+++ static/js/app.js
@@ -12,5 +12,18 @@ $(function () {
     $(this).addClass('tab2__btn--hide');
     $('.tab2__form').fadeIn(300);
   });
+
+  //  faq accord
+  $('.faq__question').on('click', function () {
+    $(this).parent().toggleClass('active');
+  });
+  //// faq accord
+
+
+  //  add ask btn
+  $('[data-js-add-ask-btn]').on('click', function () {
+    $('.faq__ask-wrap').addClass('active');
+  });
+  ////  add ask btn
 });
 //# sourceMappingURL=app.js.map
