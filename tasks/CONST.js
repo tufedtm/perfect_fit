8a703691ef3b54cc1ea213dfbf7db345764c161f
tufedtm <tufedtm@gmail.com>
2016-08-17 14:13:39 +0300

    + pop-ups

--- tasks/CONST.js
+++ tasks/CONST.js
@@ -7,7 +7,7 @@ const SRC_DIR = {
   script_src: 'src/static/js/*.js',
   style_src: 'src/static/styles/app.styl',
   style_watch: 'src/static/styles/**/*.styl',
-  template_src: 'src/*.html',
+  template_src: 'src/**/*.html',
 };
 
 module.exports = SRC_DIR;
