80cab8bc2e3a039ed7da2f3bb65131d0db6f45a6
tufedtm <tufedtm@gmail.com>
2016-08-17 14:26:46 +0300

    * fix gulp template task

--- tasks/watch.js
+++ tasks/watch.js
@@ -6,5 +6,5 @@ import SRC_DIR from './CONST'
 gulp.task('watch', function () {
   gulp.watch(SRC_DIR.style_watch, ['style']);
   gulp.watch(SRC_DIR.script_src, ['script']);
-  gulp.watch(SRC_DIR.template_src, ['template']);
+  gulp.watch(SRC_DIR.template_watch, ['template']);
 });
